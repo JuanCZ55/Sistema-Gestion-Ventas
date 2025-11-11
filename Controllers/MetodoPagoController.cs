63ba1b7ed3db6095895fee7cbc4de99eca710f26
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-11-11 15:50:01 -0300

    Añadió la funcionalidad CRUD para la gestión de métodos de pago

---
+++ Controllers/MetodoPagoController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaGestionVentas.Data;
+using SistemaGestionVentas.Models;
+
+namespace SistemaGestionVentas.Controllers
+{
+    public class MetodoPagoController : Controller
+    {
+        private readonly Context _context;
+
+        public MetodoPagoController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: MetodoPagoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MetodoPago.ToListAsync());
+        }
+
+        // GET: MetodoPagoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var metodoPago = await _context.MetodoPago.FirstOrDefaultAsync(m => m.Id == id);
+            if (metodoPago == null)
+            {
+                return NotFound();
+            }
+
+            return View(metodoPago);
+        }
+
+        // GET: MetodoPagoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MetodoPagoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Estado")] MetodoPago metodoPago)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(metodoPago);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(metodoPago);
+        }
+
+        // GET: MetodoPagoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var metodoPago = await _context.MetodoPago.FindAsync(id);
+            if (metodoPago == null)
+            {
+                return NotFound();
+            }
+            return View(metodoPago);
+        }
+
+        // POST: MetodoPagoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(
+            int id,
+            [Bind("Id,Nombre,Estado")] MetodoPago metodoPago
+        )
+        {
+            if (id != metodoPago.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(metodoPago);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MetodoPagoExists(metodoPago.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(metodoPago);
+        }
+
+        // GET: MetodoPagoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var metodoPago = await _context.MetodoPago.FirstOrDefaultAsync(m => m.Id == id);
+            if (metodoPago == null)
+            {
+                return NotFound();
+            }
+
+            return View(metodoPago);
+        }
+
+        // POST: MetodoPagoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var metodoPago = await _context.MetodoPago.FindAsync(id);
+            if (metodoPago != null)
+            {
+                _context.MetodoPago.Remove(metodoPago);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MetodoPagoExists(int id)
+        {
+            return _context.MetodoPago.Any(e => e.Id == id);
+        }
+    }
+}
