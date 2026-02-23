a4f6d6768a80caf57148cadec19728aacfd973c2
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2026-02-23 19:29:39 -0300

    Añade migración para limpiar datos de AjusteStock y Venta

---
+++ Migrations/20260223221236_CleanDashboardData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaGestionVentas.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanDashboardData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                -- Borrar AjusteStock (cascada borra AjusteStockDetalle)
+                DELETE FROM ""AjusteStock"";
+                
+                -- Borrar Venta (cascada borra DetalleVenta)
+                DELETE FROM ""Venta"";
+            "
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) { }
+    }
+}
